99959861a069d5a45a33388b6a338690878e54a7
Akeruz <57169766+Akeruz@users.noreply.github.com>
2022-03-06 12:02:00 -0400

    se le añade efecto bomba atomica precionando el boton B

--- main.ts
+++ main.ts
@@ -10,6 +10,7 @@ sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, oth
 controller.B.onEvent(ControllerButtonEvent.Released, function () {
     projectile2 = sprites.createProjectileFromSprite(assets.image`bomb`, mySprite, 0, -50)
     projectile2.ax = 100
+    projectile2.startEffect(effects.warmRadial)
     sprites.destroyAllSpritesOfKind(SpriteKind.Enemy, effects.fire, 500)
     music.beamUp.play()
 })
@@ -38,11 +39,6 @@ game.onUpdateInterval(1000, function () {
     misil.x = randint(0, scene.screenWidth())
     misil.setKind(SpriteKind.Enemy)
 })
-game.onUpdateInterval(1000, function () {
-    alien = sprites.createProjectileFromSide(assets.image`ufo`, 0, 20)
-    alien.x = randint(0, scene.screenWidth())
-    alien.setKind(SpriteKind.Enemy)
-})
 forever(function () {
     music.playMelody("B - A - F E F D ", 214)
 })
@@ -50,4 +46,9 @@ forever(function () {
     if (info.score() == 111) {
         game.over(true, effects.confetti)
     }
+})
+game.onUpdateInterval(6000, function () {
+    alien = sprites.createProjectileFromSide(assets.image`ufo`, 0, 20)
+    alien.x = randint(0, scene.screenWidth())
+    alien.setKind(SpriteKind.Enemy)
 })
